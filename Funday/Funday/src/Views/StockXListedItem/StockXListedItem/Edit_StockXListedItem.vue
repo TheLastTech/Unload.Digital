f4c3cabfbc6cceefd83bf76ae1f0bd9bd6046a68
thelasttech <3Z5IMPaEIoHg>
2020-01-24 14:52:04 -0600

    extra shitasd

--- Funday/Funday/src/Views/StockXListedItem/StockXListedItem/Edit_StockXListedItem.vue
+++ Funday/Funday/src/Views/StockXListedItem/StockXListedItem/Edit_StockXListedItem.vue
@@ -11,7 +11,7 @@
 
         >
             <template slot="cell(When)" slot-scope="data">
-                {{new Date(data.item.When).toLocaleDateString()}}
+                {{new Date(data.item.When).toLocaleDateString()}} {{new Date(data.item.When).toLocaleTimeString()}}
 
             </template>
 
