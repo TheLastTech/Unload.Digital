6a73fd0889dee85231a825f7a756337b7f229a63
thelasttech <3Z5IMPaEIoHg>
2020-01-24 13:55:35 -0600

    Fixs Multithread

--- Funday/Funday.ServiceInterface/StockxListingGetter.cs
+++ Funday/Funday.ServiceInterface/StockxListingGetter.cs
@@ -34,12 +34,17 @@ namespace Funday.ServiceInterface
                 try
                 {
                     var Sql = Db.From<StockXAccount>().Where(A => (A.AccountThread == null || A.AccountThread.Length == 0) && A.Verified && ((A.Active && !A.Disabled) && A.NextAccountInteraction <= DateTime.Now)).OrderBy(A => A.NextAccountInteraction).Take(1);
-                   var TotalUpdated = Db.UpdateOnly(() => new StockXAccount() { AccountThread = ThreadName }, Sql);
-
-                 //   if (TotalUpdated == 0)
-                //    {
-                   //     return null;
-                   // }
+                    var Item = Db.Single(Sql);
+                    if(Item == null)
+                    {
+                        return null;
+                    }
+                   var TotalUpdated = Db.UpdateOnly(() => new StockXAccount() { AccountThread = ThreadName },A=>A.Id == Item.Id && (A.AccountThread == null || A.AccountThread.Length == 0));
+              
+                    if (TotalUpdated == 0)
+                    {
+                        return null;
+                    }
                     return Db.Single(Db.From<StockXAccount>().Where(A =>  A.Verified && A.AccountThread == ThreadName && (A.Active && !A.Disabled)));
                 }
                 catch (Exception ex)
